482fdf8fcb1f0f52795a63f61c49bb3003ae438a
Danryanh7 <danielryanh7@gmail.com>
2023-12-27 17:53:06 -0500

    Adding new components

---
+++ src/app/exp/exp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exp',
+  standalone: true,
+  imports: [],
+  templateUrl: './exp.component.html',
+  styleUrl: './exp.component.scss'
+})
+export class ExpComponent {
+
+}
